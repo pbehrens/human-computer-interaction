68c6ce2578ec251e1ca51f7876651a7535d3e4b0
zherr <zherr@luc.edu>
2014-04-19 11:24:21 -0500

    Fixed threading issue: no longer creating our own threading, instead using Tkinter .after method

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from logger import Logger
 from tweet import Tweet
 from tweetprocessor import TweetProcessor
 from gui import Gui
-from multiprocessing import Process
-import time
-from threading import Timer
-from threading import Thread
 
 logger = Logger()
 api = twitteroauth.getAuthenticatedApi()
@@ -41,23 +37,13 @@ def countAndColor():
     elif (highest == 'profane'):
         gui.setColor('orange')
     # Do process again after 15 sec
-    timer.start()
-
-def startAppThread():
-    thread = Thread(target = searchEvent())
-    thread.start()
-    thread.join()
+    _job = gui.after(15000, searchEvent)
 
 def quitCallback():
-    print "quitCallback"
-    timer.cancel()
+    print "Exited."
     gui.stopGui()
-    if(thread != None):
-        thread.exit()
 
 if __name__ == '__main__':
-
-    timer = Timer(15, searchEvent)
     gui = Gui(quitCallback)
-    _job = gui.after(15000, startAppThread)
+    _job = gui.after(5000, searchEvent)
     gui.mainloop()
